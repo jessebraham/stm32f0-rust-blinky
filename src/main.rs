b3dd6ce7fd358ea388068eed5c12032f28ee047f
Jesse Braham <jesse@beta7.io>
2019-01-13 08:45:51 -0800

    Use pinned version for HAL, update accompanying interface changes

--- src/main.rs
+++ src/main.rs
@@ -5,33 +5,31 @@
 use panic_halt;
 
 extern crate stm32f0xx_hal;
-use stm32f0xx_hal::delay::Delay;
-use stm32f0xx_hal::prelude::*;
-use stm32f0xx_hal::stm32;
+use stm32f0xx_hal::{delay::Delay, prelude::*, stm32};
 
 use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    if let (Some(p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
-        // (Re-)configure PA1 as output
-        let gpioa = p.GPIOA.split();
-        let mut led = gpioa.pa1.into_push_pull_output();
+    if let (Some(mut p), Some(cp)) = (stm32::Peripherals::take(), Peripherals::take()) {
+        cortex_m::interrupt::free(move |cs| {
+            // Configure clock to 8 MHz (i.e. the default) and freeze it
+            let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
 
-        // Constrain clocking registers and configure clock to 8 MHz
-        // (i.e. the default) and freeze it
-        let rcc = p.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(8.mhz()).freeze();
+            // (Re-)configure PA1 as output
+            let gpioa = p.GPIOA.split(&mut rcc);
+            let mut led = gpioa.pa1.into_push_pull_output(cs);
 
-        // Get delay provider
-        let mut delay = Delay::new(cp.SYST, clocks);
+            // Get delay provider
+            let mut delay = Delay::new(cp.SYST, &rcc);
 
-        // Toggle the LED roughly every second
-        loop {
-            led.toggle();
-            delay.delay_ms(1_000_u16);
-        }
+            // Toggle the LED roughly every second
+            loop {
+                led.toggle();
+                delay.delay_ms(1_000_u16);
+            }
+        });
     }
 
     loop {
